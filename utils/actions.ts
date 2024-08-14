821725651be7364add736da90f3599f587b7f09e
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-08-14 13:44:31 +0200

    validation for creating profile added with zod

---
+++ utils/actions.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { profileSchema } from "./schemas";
+
+export const createProfileAction = async (
+  prevState: any,
+  formData: FormData
+) => {
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = profileSchema.parse(rawData);
+    console.log("validatedFields", validatedFields);
+
+    return { message: "Profile created" };
+  } catch (error) {
+    console.log("Error in createProfileAction");
+
+    return { message: "There was an Error" };
+  }
+};
