e2f031f4f2ed950aa6038e7c486ec77a750593fa
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-09-05 11:19:38 +0200

    working on single detail page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,3 +1,4 @@
+import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import { fetchPropertyDetail } from "@/utils/actions";
 import { redirect } from "next/navigation";
 import React from "react";
@@ -9,7 +10,11 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const { baths, bedrooms, beds, guests } = property;
   const details = { baths, bedrooms, beds, guests };
 
-  return <div>page</div>;
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+    </section>
+  );
 }
 
 export default PropertyDetailsPage;
