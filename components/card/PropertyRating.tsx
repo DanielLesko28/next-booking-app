2e18c1c40c0f185f72bdb77b76c23317137e1fe4
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-10-17 10:10:16 +0200

    working on reviews and porperty ratings

--- components/card/PropertyRating.tsx
+++ components/card/PropertyRating.tsx
@@ -1,4 +1,4 @@
-// import { fetchPropertyRating } from "@/utils/actions";
+import { fetchPropertyRating } from "@/utils/actions";
 import { FaStar } from "react-icons/fa";
 
 async function PropertyRating({
@@ -8,11 +8,11 @@ async function PropertyRating({
   propertyId: string;
   inPage: boolean;
 }) {
-  const rating = 4.7;
-  const count = 1;
+  // const rating = 4.7;
+  // const count = 1;
 
-  //   const { rating, count } = await fetchPropertyRating(propertyId);
-  //   if (count === 0) return null;
+  const { rating, count } = await fetchPropertyRating(propertyId);
+  if (count === 0) return null;
   const className = `flex gap-1 items-center ${inPage ? "text-md" : "text-xs"}`;
   const countText = count === 1 ? "review" : "reviews";
   const countValue = `(${count}) ${inPage ? countText : ""}`;
