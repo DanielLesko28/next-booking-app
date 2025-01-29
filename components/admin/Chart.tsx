e80d6065e099e998a78f9725b46ff7a20e91476f
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2025-01-29 02:09:51 +0100

    fixing deployment

--- components/admin/Chart.tsx
+++ components/admin/Chart.tsx
@@ -19,7 +19,7 @@ type ChartPropsType = {
 function Chart({ data }: ChartPropsType) {
   return (
     <section className="mt-24">
-      <h1 className="text-4xl font-semibold text-center">Monthly Bookings</h1>
+      <h1 className="text-4xl font-semibold text-center">Monthly Bookings:</h1>
       <ResponsiveContainer width="100%" height={300}>
         <BarChart data={data} margin={{ top: 50 }}>
           <CartesianGrid strokeDasharray="3 3" />
